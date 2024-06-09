d9786b16d7db361e3a5132f608b060bc5eca432a
안유진 <yoojin.an0@gmail.com>
2024-06-09 22:53:11 +0900

    [feat.mod (domain/Points)] 낙관락 사용을 위한 Version 필드 및 포인트 사용시 파라미터 유효성 판단 로직 추가
    - 낙관락 사용시 데이터 정합성 유지에 필요한 버전 필드 추가
    - 포인트 사용 메서드에 대해서 파라미터로 음수의 포인트값이 들어오면 InvalidPointsException 처리

--- src/main/java/com/example/pointmanager/domain/Points.java
+++ src/main/java/com/example/pointmanager/domain/Points.java
@@ -1,15 +1,16 @@
-package com.example.pointmanager.domain.points;
+package com.example.pointmanager.domain;
 
 import com.example.pointmanager.exception.InvalidPointsException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Version;
 import lombok.*;
 
 @Entity
-@Getter @Setter
-@RequiredArgsConstructor
-@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+//@RequiredArgsConstructor
 public class Points {
 
     @Id
@@ -19,6 +20,15 @@ public class Points {
     @Column(name = "amount_of_points")
     private long amount;
 
+    @Version
+    private long version;
+
+    public Points(long userId, long amount) {
+        this.userId = userId;
+        this.amount = amount;
+    }
+
+
     public Points charge(long amountToCharge) {
         if (amountToCharge < 0) throw new InvalidPointsException("포인트 충전 불가: 유효하지 않은 값입니다.");
         long balance = this.amount + amountToCharge;
@@ -26,6 +36,7 @@ public class Points {
     }
 
     public Points use(long amountToUse) {
+        if (amountToUse < 0) throw new InvalidPointsException("포인트 사용 불가: 유효하지 않은 값입니다.");
         long balance = this.amount - amountToUse;
         if (balance < 0) throw new InvalidPointsException("포인트 사용 불가: 잔고가 부족합니다.");
         return new Points(this.userId, balance);
