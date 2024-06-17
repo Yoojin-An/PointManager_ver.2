68af313e4de407088838bed39b32359f7107684c
안유진 <yoojin.an0@gmail.com>
2024-06-18 01:51:50 +0900

    [refactor (PointTest)] 가독성을 위해 검증 메소드를 assertEquals에서 assertThat으로 변경

--- src/test/java/com/example/pointmanager/domain/PointTest.java
+++ src/test/java/com/example/pointmanager/domain/PointTest.java
@@ -1,61 +1,66 @@
 package com.example.pointmanager.domain;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PointTest {
     @Test
     public void 포인트_충전에_성공한다() {
         // given
-        long balance = 0;
-        Point point = new Point(1, balance);
+        long userId = 1L;
+        long defaultAmount = 0L;
+        long chargeAmount = 100000L;
 
         // when
-        long amountToCharge = 100000;
-        long TotalBalance = point.charge(100000).getAmount();
+        Point point = new Point(userId, defaultAmount);
+        long amount = point.charge(chargeAmount).getAmount();
 
         // then
-        assertEquals(balance + amountToCharge, TotalBalance);
+        assertThat(amount).isEqualTo(defaultAmount + chargeAmount);
     }
 
     @Test
-    public void 음수값의_포인트_입력으로_충전에_실패한다() {
+    public void 충전할_포인트가_0이하면_충전에_실패한다() {
         // given
-        Point point = new Point(1, 0);
+        long userId = 1L;
+        long defaultAmount = 0L;
+        long chargeAmount = -50;
+        Point point = new Point(userId, defaultAmount);
 
         // when & then
-        long invalidAmountToCharge = -50;
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            point.charge(invalidAmountToCharge);
+        assertThrows(IllegalArgumentException.class, () -> {
+            point.charge(chargeAmount);
         });
-        assertEquals("포인트 충전 불가: 유효하지 않은 값입니다.", exception.getMessage());
     }
 
     @Test
     public void 포인트_사용에_성공한다() {
         // given
-        long balance = 100000;
-        Point point = new Point(1, balance);
+        long userId = 1L;
+        long defaultAmount = 100000L;
+        long useAmount = 100L;
+        Point point = new Point(userId, defaultAmount);
 
         // when
-        long amountToUse = 100;
-        long totalBalance = point.use(amountToUse).getAmount();
+        long amount = point.use(useAmount).getAmount();
 
         // then
-        assertEquals(balance - amountToUse, totalBalance);
+        assertThat(amount).isEqualTo(defaultAmount - useAmount);
     }
 
     @Test
-    public void 잔고_부족으로_포인트_사용에_실패한다() {
+    public void 사용할_포인트가_사용가능한_포인트보다_크면_사용에_실패한다() {
         // given
-        Point point = new Point(1, 0);
+        long userId = 1L;
+        long defaultAmount = 0L;
+        long useAmount = 50;
+        Point point = new Point(userId, defaultAmount);
 
         // when & then
-        long invalidAmountToUse = 50;
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
-            point.use(invalidAmountToUse);
+        assertThrows(IllegalArgumentException.class, () -> {
+            point.use(useAmount);
         });
-        assertEquals("포인트 사용 불가: 잔고가 부족합니다.", exception.getMessage());
     }
 }
