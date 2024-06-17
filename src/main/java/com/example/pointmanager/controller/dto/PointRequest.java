6fe09ae58eb7bda8d65003470c1a30e6cce0e87d
안유진 <yoojin.an0@gmail.com>
2024-06-18 00:37:43 +0900

    [bugfix (PointRequest)] 기본생성자 추가
     Resolved [org.springframework.http.converter.HttpMessageNotReadableException: JSON parse error: Cannot construct instance of  (although at least one Creator exists): cannot deserialize from Object value (no delegate- or property-based Creator)]
    
    - 위와 같은 에러가 발생하여 롬복 @NoArgsConstructor 어노테이션을 통해서 기본생성자를 생성해줌

--- src/main/java/com/example/pointmanager/controller/dto/PointRequest.java
+++ src/main/java/com/example/pointmanager/controller/dto/PointRequest.java
@@ -1,8 +1,10 @@
 package com.example.pointmanager.controller.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class PointRequest {
     private long amount;
 
