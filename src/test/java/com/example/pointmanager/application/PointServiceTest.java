87ec772d39c604c5d2ce2dec2c8cd27a5e9952c3
안유진 <yoojin.an0@gmail.com>
2024-07-18 20:23:48 +0900

    [refactor (PointServiceTest)] Nested 클래스를 사용한 계층적 테스트 구조로 변경 등
    - 포인트 조회, 포인트 내역 조회, 포인트 충전, 포인트 사용에 대해 주석으로 구분하던 기존 방식에서 Nested 클래스를 사용하는 방식으로 변경
    - 테스트 메소드를 assertEquals()에서 assertThat().isEqualTo()로 변경하여 가독성 향상
    - 테스트 명칭을 명시적으로 변경(예: 포인트_사용시_잔고가_부족하면_InvalidPointsException을_던진다 -> 사용할_포인트가_가용_포인트보다_클_경우_포인트_사용에_실패한다)
    - 실패 테스트에서 에러 메세지 검증은 제외함
    - given에서 assert할 변수들을 모두 정의하도록 변경
    - AfterEach에서 성능 문제가 있는 deleteAll 메소드를 deleteAllInBatch()로 변경

--- src/test/java/com/example/pointmanager/application/PointServiceTest.java
+++ src/test/java/com/example/pointmanager/application/PointServiceTest.java
@@ -1,17 +1,13 @@
 package com.example.pointmanager.application;
 
-import com.example.pointmanager.domain.Points;
-import com.example.pointmanager.domain.PointsHistory;
-import com.example.pointmanager.repository.PointsHistoryRepository;
-import com.example.pointmanager.repository.PointsRepository;
-import com.example.pointmanager.exception.InvalidPointsException;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.example.pointmanager.domain.Point;
+import com.example.pointmanager.domain.PointHistory;
+import com.example.pointmanager.repository.PointHistoryRepository;
+import com.example.pointmanager.repository.PointRepository;
+import com.example.pointmanager.exception.InvalidPointException;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,239 +15,225 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-@ActiveProfiles("pessimistic")
-public class PointsServiceTest {
-    private final PointsService pointsService;
-    private final PointsRepository pointsRepository;
-    private final PointsHistoryRepository pointsHistoryRepository;
+public class PointServiceTest {
+
     @Autowired
-    public PointsServiceTest(PointsService pointsService,
-                             PointsRepository pointsRepository,
-                             PointsHistoryRepository pointsHistoryRepository) {
-        this.pointsService = pointsService;
-        this.pointsRepository = pointsRepository;
-        this.pointsHistoryRepository = pointsHistoryRepository;
-    }
+    private PointService pointService;
+
+    @Autowired
+    private PointRepository pointRepository;
+
+    @Autowired
+    private PointHistoryRepository pointHistoryRepository;
 
     @BeforeEach
-    public void before() {
-        // 1번 유저의 잔고에 3번 충전, 1번 사용 결과 10000 포인트가 있는 상황을 가정
-        pointsRepository.save(new Points(1, 10000));
-        pointsHistoryRepository.saveAndFlush(PointsHistory.of(1, 10000, PointsHistory.TransactionType.CHARGE));
-        pointsHistoryRepository.saveAndFlush(PointsHistory.of(1, 10000, PointsHistory.TransactionType.CHARGE));
-        pointsHistoryRepository.saveAndFlush(PointsHistory.of(1, 10000, PointsHistory.TransactionType.CHARGE));
-        pointsHistoryRepository.saveAndFlush(PointsHistory.of(1, 20000, PointsHistory.TransactionType.USE));
+    void before() {
+        // 초기 값으로 10000 포인트를 가진 1번 유저가 10000 포인트 3번 충전, 30000 포인트 1번 사용 결과 잔고에 10000 포인트가 있는 상황을 가정
+        pointRepository.save(new Point(1L, 10000L));
+        pointHistoryRepository.saveAndFlush(PointHistory.of(1L, 10000L, PointHistory.TransactionType.CHARGE));
+        pointHistoryRepository.saveAndFlush(PointHistory.of(1L, 10000L, PointHistory.TransactionType.CHARGE));
+        pointHistoryRepository.saveAndFlush(PointHistory.of(1L, 10000L, PointHistory.TransactionType.CHARGE));
+        pointHistoryRepository.saveAndFlush(PointHistory.of(1L, 30000L, PointHistory.TransactionType.USE));
     }
 
     @AfterEach
-    public void after() {
-        pointsRepository.deleteAll();
-        pointsHistoryRepository.deleteAll();
-    }
-
-    /**
-     * 유저 등록 테스트
-     */
-    @Test
-    void 유저_등록에_성공한다() {
-        // given: 아이디
-        long userId = 1L;
-
-        // when: 유저 등록
-        long enrolledId = pointsService.enrollUser(userId).getUserId();
-
-        // then: 생성한 아이디 확인
-        assertEquals(1, enrolledId);
+    void after() {
+        pointRepository.deleteAllInBatch();
+        pointHistoryRepository.deleteAllInBatch();
     }
 
 
-    /**
-     * 포인트 조회 테스트
-     */
-    @Test
-    public void 포인트_조회에_성공한다() {
-        // given: 포인트 이용 이력이 있는 아이디
-        long userId = 1L;
+    @DisplayName("포인트 조회 테스트")
+    @Nested
+    class FindPointTest {
+        @Test
+        void 포인트_조회에_성공한다() {
+            // given
+            long userId = 1L;
+            long expectedAmount = 10000L;
 
-        // when: 포인트 조회
-        long points = pointsService.findPoints(userId).getAmount();
+            // when
+            long point = pointService.findPoint(userId).getAmount();
 
-        // then: 조회한 값이 잔고와 일치하는지 검증
-        assertEquals(10000, points);
+            // then
+            assertThat(point).isEqualTo(expectedAmount);
+        }
+
+        @Test
+        void 유효하지_않은_아이디에_대한_포인트_조회는_실패한다() {
+            // given
+            long userId = 0L;
+
+            // when & then
+            assertThrows(InvalidPointException.class, () -> {
+                pointService.findPoint(userId);
+            });
+        }
+
+        @Test
+        void 포인트_이용_내역이_없는_경우_조회에_실패한다() {
+            // given
+            long userId = 2L;
+
+            // when & then
+            assertThrows(InvalidPointException.class, () -> {
+                pointService.findPoint(userId);
+            });
+        }
     }
 
-    @Test
-    void 존재할_수_없는_아이디에_대한_포인트_조회는_실패한다() {
-        // given: 유효하지 않은 아이디
-        long userId = 0L;
 
-        // when & then: 유효하지 않은 아이디로 포인트 조회한 결과
-        // InvalidPointsException이 발생하고 예외 메세지가 예상한 바와 같음을 검증
-        InvalidPointsException exception = assertThrows(InvalidPointsException.class, () -> {
-            pointsService.findPoints(userId);
-        });
-        assertEquals("userId가 존재하지 않습니다.", exception.getMessage());
+    @DisplayName("포인트 이용 내역 조회 테스트")
+    @Nested
+    class FindPointHistoriesTest {
+        @Test
+        void 포인트_이용_내역_조회에_성공한다() {
+            // given
+            long userId = 1L;
+            int consumeCount = 4;
+            long firstChargeAmount = 10000L;
+            PointHistory.TransactionType thirdTransactionType = PointHistory.TransactionType.CHARGE;
+
+            // when
+            List<PointHistory> pointHistory = pointService.findPointHistories(userId);
+
+            // then
+            assertThat(pointHistory.size()).isEqualTo(consumeCount);
+            assertThat(pointHistory.get(0).getAmount()).isEqualTo(firstChargeAmount);
+            assertThat(pointHistory.get(2).getTransactionType()).isEqualTo(thirdTransactionType);
+        }
+
+        @Test
+        void 유효하지_않은_아이디에_대한_포인트_내역_조회는_실패한다() {
+            // given
+            long userId = -1L;
+
+            // when & then
+            assertThrows(InvalidPointException.class, () -> {
+                pointService.findPointHistories(userId);
+            });
+        }
+
+        @Test
+        void 포인트_이용_내역이_없는_경우_내역_조회에_실패한다() {
+            // given
+            long userId = 4L;
+
+            // when & then
+            assertThrows(InvalidPointException.class, () -> {
+                pointService.findPointHistories(userId);
+            });
+        }
     }
 
-    @Test
-    void 포인트_이용_내역이_없는_경우_조회에_실패한다() {
-        // given: 포인트 이용 내역이 없는 아이디
-        long userId = 2L;
 
-        // when & then: 한 번도 포인트를 충전한 적이 없는 아이디로 포인트 조회한 결과
-        // InvalidPointsException이 발생하고 예외 메세지가 예상한 바와 같음을 검증
-        RuntimeException exception = assertThrows(InvalidPointsException.class, () -> {
-            pointsService.findPoints(userId);
-        });
-        assertEquals("userId가 존재하지 않습니다.", exception.getMessage());
+    @DisplayName("포인트 충전 테스트")
+    @Nested
+    class ChargePointTest {
+        @Test
+        void 신규_유저에_대한_포인트_충전에_성공한다() {
+            // given
+            long userId = 2L;
+            long chargeAmount = 10000L;
+
+            // when
+            long point = pointService.chargePoint(userId, chargeAmount, true).getAmount();
+
+            // then
+            assertThat(point).isEqualTo(chargeAmount);
+        }
+
+        @Test
+        void 기존_유저에_대한_포인트_충전에_성공한다() {
+            // given
+            long userId = 1L;
+            long amount = 10000L;
+            long chargeAmount = 5000L;
+
+            // when
+            long point = pointService.chargePoint(userId, chargeAmount, true).getAmount();
+
+            // then
+            assertThat(point).isEqualTo(amount + chargeAmount);
+        }
+
+        @Test
+        void 충전할_포인트가_0_이하면_충전에_실패한다() {
+            // given
+            long userId = 1L;
+            long chargeAmount = -500L;
+
+            // when & then
+            assertThrows(InvalidPointException.class, () -> {
+                pointService.chargePoint(userId, chargeAmount, true);
+            });
+        }
+
+        @Test
+        void 유효하지_않은_아이디에_대한_포인트_충전은_실패한다() {
+            // given
+            long userId = -1L;
+            long chargeAmount = 10000L;
+
+            // when & then
+            assertThrows(InvalidPointException.class, () -> {
+                pointService.chargePoint(userId, chargeAmount, true);
+            });
+        }
     }
 
-    /**
-     * 포인트 이용 내역 조회 테스트
-     */
-    @Test
-    public void 포인트사용_이력이_있는_경우_포인트_내역_조회에_성공한다() {
-        // given: 포인트 이용 이력이 있는 아이디
-        long userId = 1L;
 
-        // when: 3번 충전, 1번 사용의 이력이 있음을 가정하고 포인트 충전, 사용 이력 조회
-        List<PointsHistory> pointsHistory = pointsService.findPointsHistory(userId);
+    @DisplayName("포인트 사용 테스트")
+    @Nested
+    class PointUseTest {
+        @Test
+        void 포인트_사용에_성공한다() {
+            // given
+            long userId = 1L;
+            long amount = 10000L;
+            long useAmount = 5000L;
 
-        // then: 잔고, 충전/사용 횟수, 충전/사용 타입 검증
-        assertEquals(10000, pointsHistory.get(0).getAmount());
-        assertEquals(4, pointsHistory.size());
-        assertEquals(PointsHistory.TransactionType.CHARGE, pointsHistory.get(2).getTransactionType());
-    }
+            // when
+            long point = pointService.usePoint(userId, useAmount, true).getAmount();
 
-    @Test
-    public void 사용_이력이_없는_경우_포인트_내역_조회에_실패한다() {
-        // given: 포인트 이용 이력이 없는 아이디
-        long userId = 4L;
+            // then
+            assertThat(point).isEqualTo(amount - useAmount);
+        }
 
-        // when: 한 번도 포인트를 충전한 적이 없는 아이디로 포인트 내역 조회한 결과
-        // InvalidPointsException이 발생하고 예외 메세지가 예상한 바와 같음을 검증
-        InvalidPointsException exception = assertThrows(InvalidPointsException.class, () -> {
-            pointsService.findPointsHistory(userId);
-        });
+        @Test
+        void 사용할_포인트가_가용_포인트보다_클_경우_포인트_사용에_실패한다() {
+            // given
+            long userId = 1L;
+            long useAmount = 50000L;
 
-        assertEquals("userId가 존재하지 않습니다.", exception.getMessage());
-    }
+            // when & then
+            assertThrows(InvalidPointException.class, () -> {
+                pointService.usePoint(userId, useAmount, true);
+            });
+        }
 
-    @Test
-    void 존재할_수_없는_아이디에_대한_포인트_내역_조회는_실패한다() {
-        // given: 유효하지 않은 아이디
-        long userId = -1L;
+        @Test
+        void 사용할_포인트가_0이하면_포인트_사용에_실패한다() {
+            // given
+            long userId = 1L;
+            long useAmount = -10000L;
 
-        // when & then: 유효하지 않은 아이디로 포인트 내역 조회한 결과
-        // InvalidPointsException이 발생하고 예외 메세지가 예상한 바와 같음을 검증
-        InvalidPointsException exception = assertThrows(InvalidPointsException.class, () -> {
-            pointsService.findPointsHistory(userId);
-        });
-        assertEquals("userId가 존재하지 않습니다.", exception.getMessage());
-    }
+            // when & then
+            assertThrows(InvalidPointException.class, () -> {
+                pointService.usePoint(userId, useAmount, true);
+            });
+        }
 
-    /**
-     * 포인트 충전 테스트
-     */
-    @Test
-    public void 포인트_충전에_성공한다() throws InterruptedException {
-        // given: 포인트 이용 이력이 있는 아이디와 충전할 포인트
-        long userId = 1L;
-        long balance = 10000;
-        long amountToCharge = 5000;
+        @Test
+        void 유효하지_않은_아이디에_대한_포인트_사용은_실패한다() {
+            // given
+            long userId = -1L;
+            long useAmount = 10000L;
 
-        // when: 포인트 충전
-        long points = pointsService.chargePoints(userId, amountToCharge).getAmount();
-
-        // then: 잔고에 충전할 금액을 더한 값이 충전 결과와 같은지 검증
-        assertThat(points).isEqualTo(balance + amountToCharge);
-    }
-
-    @Test
-    public void 충전할_포인트가_음수이거나_0이면_충전에_실패한다() {
-        // given: 포인트 이용 이력이 있는 아이와 유효하지 않은 충전 포인트
-        long userId = 1L;
-        long balance = 10000L;
-        long amountToCharge = -500L;
-
-        // when: -500 포인트 충전으로 예외 발생
-        InvalidPointsException exception = assertThrows(InvalidPointsException.class, () -> {
-            pointsService.chargePoints(userId, amountToCharge);
-        });
-
-        // then: InvalidPointsException 발생 메세지 및 예외 발생 전후 포인트 일치 상태 검증
-        assertEquals("포인트 충전 불가: 유효하지 않은 값입니다.", exception.getMessage());
-        assertEquals(balance, pointsService.findPoints(userId).getAmount());
-    }
-
-    @Test
-    void 존재할_수_없는_아이디에_대한_포인트_충전은_실패한다() {
-        // given: 유효하지 않은 아이디와 유효한 포인트
-        long userId = -1L;
-        long amount = 10000L;
-
-        // when & then: 유효하지 않은 아이디로 포인트 조회한 결과
-        // RuntimeException이 발생하고 예외 메세지가 예상한 바와 같음을 검증
-        InvalidPointsException exception = assertThrows(InvalidPointsException.class, () -> {
-            pointsService.chargePoints(userId, amount);
-        });
-        assertEquals("userId가 존재하지 않습니다.", exception.getMessage());
-    }
-
-    /**
-     * 포인트 사용 테스트
-     */
-    @Test
-    public void 포인트_사용에_성공한다() throws InterruptedException {
-        // given: 포인트 사용 이력이 있는 아이디와 유효한 포인트
-        long balance = 10000;
-        long amountToUse = 5000;
-
-        // when: 포인트 사용
-        long points = pointsService.usePoints(1, amountToUse).getAmount();
-
-        // then: 잔고에 사용한 금액을 뺀 값이 사용 결과와 같은지 검증
-        assertEquals(balance - amountToUse, points);
-    }
-
-    @Test
-    void 포인트_사용시_잔고가_부족하면_InvalidPointsException을_던진다() {
-        // given: 잔고가 부족한 상황
-        long userId = 1L;
-        long amountToUse = 50000L;
-
-        // when & then: 초기 포인트 10000인 상황에서 50000 포인트 사용 시 InvalidPointsException 예외 발생 및 예외 메세지 검증
-        RuntimeException exception = assertThrows(InvalidPointsException.class, () -> {
-            pointsService.usePoints(userId, amountToUse);
-        });
-        assertEquals("포인트 사용 불가: 잔고가 부족합니다.", exception.getMessage());
-    }
-
-    @Test
-    void 사용할_포인트가_0이거나_음수이면_포인트_사용에_실패한다() {
-        // given: 포인트 사용 이력이 있는 아이디와 유효하지 않은 포인트
-        long userId = 1L;
-        long amountToUse = -10000L;
-
-        // when & then: 유효하지 않은 포인트 사용을 시도한 결과
-        // InvalidPointsException이 발생하고 예외 메세지가 예상한 바와 같음을 검증
-        InvalidPointsException exception = assertThrows(InvalidPointsException.class, () -> {
-            pointsService.usePoints(userId, amountToUse);
-        });
-        assertEquals("포인트 사용 불가: 유효하지 않은 값입니다.", exception.getMessage());
-    }
-
-    @Test
-    void 존재할_수_없는_아이디에_대한_포인트_사용은_실패한다() {
-        // given: 유효하지 않은 아이디와 유효한 포인트
-        long inValidId = -1L;
-        long amountToUse = 10000L;
-
-        // when & then: 유효하지 않은 아이디의 포인트 사용을 시도한 결과
-        InvalidPointsException exception = assertThrows(InvalidPointsException.class, () -> {
-            pointsService.chargePoints(inValidId, amountToUse);
-        });
-        // InvalidPointsException 발생하고 예외 메세지가 예상한 바와 같음을 검증
-        assertEquals("userId가 존재하지 않습니다.", exception.getMessage());
+            // when & then
+            assertThrows(InvalidPointException.class, () -> {
+                pointService.chargePoint(userId, useAmount, true);
+            });
+        }
     }
 }
